acd200b119cf226976621be39cda551b1b25a065
Rahul Mulik <rahulmulik9@gmail.com>
2024-03-31 22:47:27 +0530

    Initial back end commit spring initializer

---
+++ backend/src/main/java/com/rahul/BankWebApp/BankWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.rahul.BankWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankWebAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rahul/BankWebApp/BankWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.BankWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
