26b8c98aa0e21c99fdf4ca2d472158afa18ef861
Rahul Mulik <rahulmulik9@gmail.com>
2024-04-01 17:45:08 +0530

    InMemoryUserDetailsManager to configure this added Password encoder

--- backend/src/main/java/com/rahul/BankWebApp/config/ProjectSecurityConfig.java
+++ backend/src/main/java/com/rahul/BankWebApp/config/ProjectSecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -25,7 +27,6 @@ public class ProjectSecurityConfig {
 
     }
 
-
    @Bean
     public InMemoryUserDetailsManager userDetailsService() {
         UserDetails admin = User.withUsername("admin")
@@ -38,6 +39,11 @@ public class ProjectSecurityConfig {
                 .build();
         return new InMemoryUserDetailsManager(admin, user);
 
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 
 }
