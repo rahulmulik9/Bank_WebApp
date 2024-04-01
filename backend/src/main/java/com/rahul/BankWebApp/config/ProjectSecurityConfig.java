e00b3781d05add26e610e401433c4aa2c31e9cd8
Rahul Mulik <rahulmulik9@gmail.com>
2024-04-01 17:35:53 +0530

    InMemoryUserDetailsManager

--- backend/src/main/java/com/rahul/BankWebApp/config/ProjectSecurityConfig.java
+++ backend/src/main/java/com/rahul/BankWebApp/config/ProjectSecurityConfig.java
@@ -4,6 +4,10 @@ package com.rahul.BankWebApp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -20,4 +24,20 @@ public class ProjectSecurityConfig {
         return http.build();
 
     }
+
+
+   @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails admin = User.withUsername("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+        UserDetails user = User.withUsername("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+
+    }
+
 }
