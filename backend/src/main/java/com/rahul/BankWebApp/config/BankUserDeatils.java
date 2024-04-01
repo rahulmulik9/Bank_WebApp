5e2d80a74c9a9f3a7599af472a7792bba2b9d9f0
Rahul Mulik <rahulmulik9@gmail.com>
2024-04-01 22:31:06 +0530

    EazyBankUsernamePwdAuthenticationProvider : This is custom provider which will check if user is present in the system or not and return AuthenticationToken

--- backend/src/main/java/com/rahul/BankWebApp/config/BankUserDeatils.java
+++ backend/src/main/java/com/rahul/BankWebApp/config/BankUserDeatils.java
@@ -1,37 +1,37 @@
-package com.rahul.BankWebApp.config;
-
-import com.rahul.BankWebApp.entity.Customer;
-import com.rahul.BankWebApp.repository.CustomerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Service
-public class BankUserDeatils implements UserDetailsService {
-    @Autowired
-    CustomerRepository customerRepository;
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        String userName, password;
-        List<GrantedAuthority> authorities;
-        List<Customer> customer = customerRepository.findByEmail(username);
-        if (customer.size() == 0) {
-            throw new UsernameNotFoundException("User details not found for the user : " + username);
-        } else{
-            userName = customer.get(0).getEmail();
-            password = customer.get(0).getPwd();
-            authorities = new ArrayList<>();
-            authorities.add(new SimpleGrantedAuthority(customer.get(0).getRole()));
-        }
-        //this user is from Spring security
-        return new User(userName,password,authorities);
-    }
-}
+//package com.rahul.BankWebApp.config;
+//
+//import com.rahul.BankWebApp.entity.Customer;
+//import com.rahul.BankWebApp.repository.CustomerRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Service
+//public class BankUserDeatils implements UserDetailsService {
+//    @Autowired
+//    CustomerRepository customerRepository;
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        String userName, password;
+//        List<GrantedAuthority> authorities;
+//        List<Customer> customer = customerRepository.findByEmail(username);
+//        if (customer.size() == 0) {
+//            throw new UsernameNotFoundException("User details not found for the user : " + username);
+//        } else{
+//            userName = customer.get(0).getEmail();
+//            password = customer.get(0).getPwd();
+//            authorities = new ArrayList<>();
+//            authorities.add(new SimpleGrantedAuthority(customer.get(0).getRole()));
+//        }
+//        //this user is from Spring security
+//        return new User(userName,password,authorities);
+//    }
+//}
---
+++ backend/src/main/java/com/rahul/BankWebApp/config/EazyBankUsernamePwdAuthenticationProvider.java
@@ -0,0 +1,52 @@
+package com.rahul.BankWebApp.config;
+
+import com.rahul.BankWebApp.entity.Customer;
+import com.rahul.BankWebApp.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class EazyBankUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        List<Customer> customer = customerRepository.findByEmail(username);
+        if (customer.size() > 0) {
+            if (passwordEncoder.matches(pwd, customer.get(0).getPwd())) {
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority(customer.get(0).getRole()));
+                return new UsernamePasswordAuthenticationToken(username, pwd, authorities);
+            } else {
+                throw new BadCredentialsException("Invalid password!");
+            }
+        }else {
+            throw new BadCredentialsException("No user registered with this details!");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        //this tell that support Username and password authentication token which use username and password
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+
+}
