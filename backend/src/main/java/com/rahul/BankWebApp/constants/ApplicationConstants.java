52ae5dfae5df9eee2f3a64440284b4ec224c8ceb
Rahul Mulik <rahulmulik9@gmail.com>
2024-04-03 22:36:15 +0530

    Added some constant value

---
+++ backend/src/main/java/com/rahul/BankWebApp/constants/ApplicationConstants.java
@@ -0,0 +1,8 @@
+package com.rahul.BankWebApp.constants;
+
+public class ApplicationConstants {
+
+    public static final String ERROR_MSG = "An unknown error occured";
+    public static final String SUCCESS_MSG = "Request Successfully processed";
+
+}
