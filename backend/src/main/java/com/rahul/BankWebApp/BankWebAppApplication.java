1ee436fdf6815bd9a71524643568a9d77d0ab1c1
Rahul Mulik <rahulmulik9@gmail.com>
2024-04-05 23:32:40 +0530

    modified the application.properties Added enableWebSecurity annotation

--- backend/src/main/java/com/rahul/BankWebApp/BankWebAppApplication.java
+++ backend/src/main/java/com/rahul/BankWebApp/BankWebAppApplication.java
@@ -2,8 +2,10 @@ package com.rahul.BankWebApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity(debug = true)
 public class BankWebAppApplication {
 
 	public static void main(String[] args) {
