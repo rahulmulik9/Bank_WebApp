9926326677528689f11aa33ce50839945c1d5b68
Rahul Mulik <rahulmulik9@gmail.com>
2024-04-06 19:06:46 +0530

    Added session Management Cors configured for exposed header Authorization

--- backend/src/main/java/com/rahul/BankWebApp/config/ProjectSecurityConfig.java
+++ backend/src/main/java/com/rahul/BankWebApp/config/ProjectSecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
@@ -38,6 +39,7 @@ public class ProjectSecurityConfig {
                         config.setAllowedMethods(Collections.singletonList("*"));
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setExposedHeaders(Arrays.asList("Authorization"));
                         config.setMaxAge(3600L);
                         return config;
                     }
